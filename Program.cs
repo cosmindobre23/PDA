605dc8a88aa4096928da840c09323bf36a9ed1a0
DESKTOP-GVHP3NQ\cosmi <cosmindobre96@yahoo.com>
2018-04-25 23:21:42 +0300

    1st

--- Program.cs
+++ Program.cs
@@ -10,31 +10,35 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            using (new MPI.Environment(ref args))
+            using ( new MPI.Environment(ref args))
             {
-                int sum = 0;
-                int[] v= { 1,2,3,4,5,6,7,8,9,10};
+                Random rand = new Random();
+                int number = rand.Next(100, 1000);
                 int pnr = Communicator.world.Size;
                 var com = Communicator.world;
                 int rnr = com.Rank;
                 if (rnr != 0)
                 {
-
-                    for(int i=rnr; i<10; i = i + pnr - 1)
-                    {
-                        sum += v[i];
-                    }
-                    com.Send(sum, 0, rnr);
+                    com.Send(number, 0, rnr);
                 }
                 else
                 {
-                    var value = 0;
+                    int max = number, rnk = 0;
+                    int value = 0;
                     for (int i = 1; i < pnr; i++)
-                    { 
+                    {
                         value = com.Receive<int>(i, i);
-                        sum += value;
+                        if (max < value)
+                        {
+                            max = value;
+                            rnk = i;
+                        }
+                        else if (max == value)
+                        {
+                            rnk = i;
+                        }
                     }
-                    Console.WriteLine("suma este {0}", sum);
+                    Console.WriteLine("Master este procesul nr {0} cu numarul {1}", rnk, max);
                 }
             }
         }
